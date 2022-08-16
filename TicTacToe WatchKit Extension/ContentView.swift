672bee4bef5f6ba2cdc4290402f7cd5b0a090d17
Tristan Camejo <contact@tristancamejo.com>
2022-08-16 10:05:43 +1000

    impl: app

--- TicTacToe WatchKit Extension/ContentView.swift
+++ TicTacToe WatchKit Extension/ContentView.swift
@@ -8,9 +8,128 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var board: [[String]] = [["", "", ""], ["", "", ""], ["", "", ""]]
+    @State var turn: String = "X"
+    @State var winner: String = ""
+    @State var gameOver: Bool = false
+    
+    func reset() {
+        board = [["", "", ""], ["", "", ""], ["", "", ""]]
+        turn = "X"
+        winner = ""
+        gameOver = false
+    }
+    
+    func checkWinner() {
+        if board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] != "" {
+            winner = board[0][0]
+            gameOver = true
+        } else if board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] != "" {
+            winner = board[1][0]
+            gameOver = true
+        } else if board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] != "" {
+            winner = board[2][0]
+            gameOver = true
+        } else if board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[0][0] != "" {
+            winner = board[0][0]
+            gameOver = true
+        } else if board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[0][1] != "" {
+            winner = board[0][1]
+            gameOver = true
+        } else if board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[0][2] != "" {
+            winner = board[0][2]
+            gameOver = true
+        } else if board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != "" {
+            winner = board[0][0]
+            gameOver = true
+        } else if board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != "" {
+            winner = board[0][2]
+            gameOver = true
+        }
+    }
+    
+    func takeTurn(row: Int, col: Int) {
+        WKInterfaceDevice.current().play(.success)
+        if board[row][col] == "" {
+            board[row][col] = turn
+            if turn == "X" {
+                turn = "O"
+            } else {
+                turn = "X"
+            }
+        }
+        checkWinner()
+    }
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        
+        if (winner != "") {
+            VStack {
+                Text("\(winner) won!")
+                    .padding(.bottom, 10)
+                Button(action: {
+                    reset()
+                }) {
+                    Text("Play again?")
+                }
+            }}
+        
+        HStack {
+            VStack {
+                Button(action: {
+                    takeTurn(row: 0, col: 0)
+                    //update ui state
+                    
+                    
+                }, label: {
+                    Text(board[0][0])
+                })
+                Button(action: {
+                    takeTurn(row: 1, col: 0)
+                }, label: {
+                    Text(board[1][0])
+                })
+                Button(action: {
+                    takeTurn(row: 2, col: 0)
+                }, label: {
+                    Text(board[2][0])
+                })
+            }
+            VStack {
+                Button(action: {
+                    takeTurn(row: 0, col: 1)
+                }, label: {
+                    Text(board[0][1])
+                })
+                Button(action: {
+                    takeTurn(row: 1, col: 1)
+                }, label: {
+                    Text(board[1][1])
+                })
+                Button(action: {
+                    takeTurn(row: 2, col: 1)
+                }, label: {
+                    Text(board[2][1])
+                })
+            }
+            VStack {
+                Button(action: {
+                    takeTurn(row: 0, col: 2)
+                }, label: {
+                    Text(board[0][2])
+                })
+                Button(action: {
+                    takeTurn(row: 1, col: 2)
+                }, label: {
+                    Text(board[1][2])
+                })
+                Button(action: {
+                    takeTurn(row: 2, col: 2)
+                }, label: {
+                    Text(board[2][2])
+                })
+            }
+        }
     }
 }
 
